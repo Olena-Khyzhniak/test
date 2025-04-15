32b44064edcb5a20b81e8f6d016f1587d90e1005
Olena Khyzhniak <helenkhyzhniak@gmail.com>
2025-04-15 10:55:06 +0100

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,20 @@
+module ok.test {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens ok.test to javafx.fxml;
+    opens ok.test.Controller to javafx.fxml;
+
+    exports ok.test;
+    exports ok.test.Controller to javafx.fxml;
+
+}
